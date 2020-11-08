fc9d9ba4f99770e3e62edcfe1e0034cd457f9f12
jhoo0013 <jhoo0013@student.monash.edu>
2020-11-08 22:18:56 +1100

    added new folders

---
+++ templates/Categories/view.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Category $category
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('Edit Category'), ['action' => 'edit', $category->id], ['class' => 'side-nav-item']) ?>
+            <?= $this->Form->postLink(__('Delete Category'), ['action' => 'delete', $category->id], ['confirm' => __('Are you sure you want to delete # {0}?', $category->id), 'class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('List Categories'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('New Category'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="categories view content">
+            <h3><?= h($category->name) ?></h3>
+            <table>
+                <tr>
+                    <th><?= __('Name') ?></th>
+                    <td><?= h($category->name) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Id') ?></th>
+                    <td><?= $this->Number->format($category->id) ?></td>
+                </tr>
+            </table>
+            <div class="related">
+                <h4><?= __('Related Products') ?></h4>
+                <?php if (!empty($category->products)) : ?>
+                <div class="table-responsive">
+                    <table>
+                        <tr>
+                            <th><?= __('Id') ?></th>
+                            <th><?= __('Name') ?></th>
+                            <th><?= __('Sale Price') ?></th>
+                            <th><?= __('Country Of Origin') ?></th>
+                            <th><?= __('Category Id') ?></th>
+                            <th class="actions"><?= __('Actions') ?></th>
+                        </tr>
+                        <?php foreach ($category->products as $products) : ?>
+                        <tr>
+                            <td><?= h($products->id) ?></td>
+                            <td><?= h($products->name) ?></td>
+                            <td><?= h($products->sale_price) ?></td>
+                            <td><?= h($products->country_of_origin) ?></td>
+                            <td><?= h($products->category_id) ?></td>
+                            <td class="actions">
+                                <?= $this->Html->link(__('View'), ['controller' => 'Products', 'action' => 'view', $products->id]) ?>
+                                <?= $this->Html->link(__('Edit'), ['controller' => 'Products', 'action' => 'edit', $products->id]) ?>
+                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'Products', 'action' => 'delete', $products->id], ['confirm' => __('Are you sure you want to delete # {0}?', $products->id)]) ?>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
